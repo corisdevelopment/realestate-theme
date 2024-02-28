5925b5aeaa189562f9318ce9aa442e6cddec9755
Kyle Sturman <kyle@summitdesignmanagement.com>
2024-02-28 17:18:57 -0600

    Added navbar and screen size indicator

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="astro/client" />
+
+// Types for ENV Vars
+interface ImportMetaEnv {
+    readonly PUBLIC_TEST: string;
+    readonly CMS_REPO_NAME: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
