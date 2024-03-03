f5d48edbe13702295c37c452bb94b7144d6ac8ba
Kyle Sturman <kyle@summitdesignmanagement.com>
2024-03-02 20:03:04 -0600

    Added styling and layout change to Navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), react()]
+  integrations: [tailwind(), sitemap(), react(), icon()]
 });
