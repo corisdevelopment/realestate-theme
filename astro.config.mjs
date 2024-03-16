579473b63c671d12e60af7ae05557cd58e007637
Kyle Sturman <kyle@summitdesignmanagement.com>
2024-03-15 19:07:47 -0500

    Add footer and changed renderer to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import react from "@astrojs/react";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), react(), icon()]
+  site: 'https://corisdevelopmentgroup.com',
+  integrations: [tailwind(), sitemap(), icon()],
+  output: 'static'
 });
