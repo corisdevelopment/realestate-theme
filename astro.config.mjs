4ad9247e62361f4493ba37d0de4fc9255c57a017
Kyle Sturman <kyle@summitdesignmanagement.com>
2024-04-14 14:05:42 -0500

    Adding in Storyblok CMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,34 @@
 import { defineConfig } from 'astro/config';
+
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import icon from "astro-icon";
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://corisdevelopmentgroup.com',
-  integrations: [tailwind(), sitemap(), icon()],
-  output: 'static'
+  integrations: [
+    
+    storyblok({
+      accessToken: "dpqGY8PFmM9rMW46GK7WjAtt",
+      bridge: true,
+      // components: {
+      //   // TODO Add Storyblok Components
+      //   page: 'storyblok/Page',
+      //   blogPost: 'storyblok/BlogPost',
+      //   blogList: 'storyblok/BlogPostList',
+      // },
+      apiOptions: {
+        region: "us",
+      }
+    }),
+    tailwind(), 
+    sitemap(), 
+    icon(),
+  ],
+  output: 'server'
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -10,3 +10,8 @@ interface ImportMetaEnv {
 interface ImportMeta {
     readonly env: ImportMetaEnv;
 }
+
+// AnimeJS Config
+declare module 'animejs/lib/anime.es.js';
+
+
