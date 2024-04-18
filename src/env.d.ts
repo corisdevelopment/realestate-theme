565e1c6cdd91ca68011ec3c187fb36c51ea9e9f6
Kyle Sturman <kyle@summitdesignmanagement.com>
2024-04-18 13:14:25 -0500

    trying out fixes

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,11 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
-// Types for ENV Vars
-interface ImportMetaEnv {
-    readonly PUBLIC_TEST: string;
-    readonly CMS_REPO_NAME: string;
-}
+// // Types for ENV Vars
+// interface ImportMetaEnv {
+//     readonly PUBLIC_TEST: string;
+//     readonly CMS_REPO_NAME: string;
+// }
 
 interface ImportMeta {
     readonly env: ImportMetaEnv;
