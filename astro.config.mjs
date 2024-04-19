fc73b232f433bed31110212b78d7872ef2703c9e
Kyle Sturman <kyle@summitdesignmanagement.com>
2024-04-18 19:22:00 -0500

    log process env

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
-console.log('hello')
+console.log('hello', process.env)
 // https://astro.build/config
 export default defineConfig({
   site: 'https://corisdevelopmentgroup.com',
