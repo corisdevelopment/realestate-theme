78ac163d3c87598873b5228ebd3d6a4f8e7571e7
Kyle Sturman <kyle@summitdesignmanagement.com>
2025-03-16 14:34:04 -0500

    updates to widen layout

--- src/lib/api/settings.ts
+++ src/lib/api/settings.ts
@@ -1,99 +1,115 @@
-import { useStoryblokApi } from '@storyblok/astro'
+import { useStoryblokApi } from "@storyblok/astro";
+import type { IndexType } from "typescript";
 
 // Get Storyblok Data
-const sbApi = useStoryblokApi()
-const { data } = await sbApi.get('cdn/stories/settings', {
-  version: 'draft',
-  resolve_links: 'url',
-})
-const phoneNumber = data?.story?.content?.company_phone
-const phoneLink = phoneNumber.replace(/-/g, '')
-const email = data?.story?.content?.company_email
-const mainNavItems = data?.story?.content?.header
-const footer = data?.story?.content?.footer
+const sbApi = useStoryblokApi();
+const { data } = await sbApi.get("cdn/stories/settings", {
+	version: "draft",
+	resolve_links: "url",
+});
+const phoneNumber = data?.story?.content?.company_phone;
+const phoneLink = phoneNumber.replace(/-/g, "");
+const email = data?.story?.content?.company_email;
+const mainNavItems = data?.story?.content?.header;
+const footer = data?.story?.content?.footer;
 
-let mainNav: any = []
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+const mainNav: any = [];
 
-mainNavItems.map((data: any) => {
-  data.menu_items.map((item: any) => {
-    if (item.sub_items.length === 0) {
-      mainNav.push({
-        title: item.title,
-        link: item.link.cached_url != 'home' ? "/"+item.link.cached_url : '/',
-      })
-    } else {
-      let subItems: any = []
-      item.sub_items.map((subItem: any) => {
-        subItems.push({
-          title: subItem.title,
-          link: "/" + subItem.link.cached_url,
-        })
-      })
-      mainNav.push({
-        title: item.title,
-        link: '#',
-        sub: subItems
-      })
-    }
-    
-  })
-})
-const footerNavItems = footer.map((item: any) => {
-  if (item.menu_items) {
-    return {
-      menu_items: item.menu_items
-    }
-  }
-}).filter(Boolean)
-
-let footerNav: any = []
-
+mainNavItems.map((data: { [key: string]: [] }) => {
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	data.menu_items?.map((item: any) => {
+		if (item.sub_items?.length === 0) {
+			mainNav.push({
+				title: item.title,
+				link:
+					item.link.cached_url !== "home" ? `/${item.link.cached_url}` : "/",
+			});
+		} else {
+			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+			const subItems: any = [];
+			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+			item.sub_items.map((subItem: any) => {
+				subItems.push({
+					title: subItem.title,
+					link: `/${subItem.link.cached_url}`,
+				});
+			});
+			mainNav.push({
+				title: item.title,
+				link: "#",
+				sub: subItems,
+			});
+		}
+	});
+});
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+const footerNavItems = footer
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	.map((item: any) => {
+		if (item.menu_items) {
+			return {
+				menu_items: item.menu_items,
+			};
+		}
+	})
+	.filter(Boolean);
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+const footerNav: any = [];
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 footerNavItems.map((data: any) => {
-  data.menu_items.map((item: any) => {
-    if (item.sub_items.length === 0) {
-      footerNav.push({
-        title: item.title,
-        link: item.link.cached_url != 'home' ? "/"+item.link.cached_url : '/',
-      })
-    } else {
-      item.sub_items.map((subItem: any) => {
-        footerNav.push({
-          title: subItem.title,
-          link: "/"+subItem.link.cached_url,
-        })
-      })
-    }
-  })
-})
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	data.menu_items.map((item: any) => {
+		if (item.sub_items.length === 0) {
+			footerNav.push({
+				title: item.title,
+				link:
+					item.link.cached_url !== "home" ? `/${item.link.cached_url}` : "/",
+			});
+		} else {
+			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+			item.sub_items.map((subItem: any) => {
+				footerNav.push({
+					title: subItem.title,
+					link: `/${subItem.link.cached_url}`,
+				});
+			});
+		}
+	});
+});
 
-const socials = footer.map((item: any) => {
-  if (item.socials) {
-    return item.socials
-  }
-}).filter(Boolean)
+const socials = footer
+	.map((item: { [key: string]: [] }) => {
+		if (item.socials) {
+			return item.socials;
+		}
+	})
+	.filter(Boolean);
 
-let socialMedia: any = []
+const socialMedia: object[] = [];
 
-socials[0].map((item: any) => {
-  socialMedia.push({
-    title: item.platform,
-    icon: "ion:logo-" + item.platform,
-    label: `visit ${item.platform} profile`,
-    link: item.link.url != '' ? item.link.url : '#'
-  })
-})
+socials[0].map((item: { [key: string]: { [key: string]: string } }) => {
+	socialMedia.push({
+		title: item.platform,
+		icon: `ion:logo-${item.platform}`,
+		label: `visit ${item.platform} profile`,
+		link: item.link.url !== "" ? item.link.url : "#",
+	});
+});
 
-let hideSocial = footer.map((item: any) => {
-  return item.hide_social
-}).filter((el: any) => el != undefined)
-hideSocial = hideSocial[0]
+let hideSocial = footer
+	.map((item: { [key: string]: unknown }) => {
+		return item.hide_social;
+	})
+	.filter((el: unknown) => el !== undefined);
+hideSocial = hideSocial[0];
 
 export {
-  phoneNumber,
-  phoneLink,
-  email,
-  mainNav,
-  footerNav,
-  socialMedia,
-  hideSocial
-}
+	phoneNumber,
+	phoneLink,
+	email,
+	mainNav,
+	footerNav,
+	socialMedia,
+	hideSocial,
+};
