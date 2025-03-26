e4dd65654bef6351ec0de1ac13d6b6f87c940d38
Kyle Sturman <kyle@summitdesignmanagement.com>
2025-03-25 21:10:44 -0500

    testing moving storyblok api call logic from each page to its own function to handle repeating code

---
+++ src/lib/api/storyAPI.ts
@@ -0,0 +1,40 @@
+import { getLiveStory, useStoryblokApi, type ISbStoriesParams } from "@storyblok/astro";
+import type { AstroGlobal } from "astro";
+
+export type TSOptions = {
+  version?: 'draft' | 'published';
+  resolve_links?: 'link' | 'url' | 'story' | '0' | '1';
+  resolve_relations?: string | string[];
+  is_startpage?: boolean;
+  content_type?: string;
+}
+/**
+ * * FUNCTION: GetStory()
+ * @param Astro | Astro global needed for getLiveStory
+ * @param storySlug | slug to get stories from
+ * @param storyOptions | object
+ * - Expects type TSOptions which takes the same types as ISbStoriesParams 
+ *   just not as many options. storyOptions should be able to overide and add
+ *   as needed. Revise or overide storyOptions type if needed.
+ * @returns story
+ */
+export async function GetStory(Astro: Readonly<AstroGlobal>, storySlug: string, storyOptions?: TSOptions) {
+  let story = null;
+  const liveStory = await getLiveStory(Astro);
+  const defaultVersion = import.meta.env.DEV ? "draft" : "published";
+
+  const options: ISbStoriesParams = {
+    version: defaultVersion,
+    resolve_links: 'url',
+    ...storyOptions,
+  }
+  
+  if (liveStory) {
+    story = liveStory;
+  } else {
+    const storyAPI = useStoryblokApi();
+    const { data } = await storyAPI.get(`cdn/stories/${storySlug}`, options );
+    story = data?.story;
+  }
+  return story;
+}
