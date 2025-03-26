fb8f45158e4b26aeac8254548458f15b6a75c248
Kyle Sturman <kyle@summitdesignmanagement.com>
2025-03-25 23:02:53 -0500

    updated pages to handle new storyAPI function with live preview

--- src/lib/api/storyAPI.ts
+++ src/lib/api/storyAPI.ts
@@ -7,6 +7,7 @@ export type TSOptions = {
   resolve_relations?: string | string[];
   is_startpage?: boolean;
   content_type?: string;
+  starts_with?: string;
 }
 /**
  * * FUNCTION: GetStory()
@@ -18,7 +19,7 @@ export type TSOptions = {
  *   as needed. Revise or overide storyOptions type if needed.
  * @returns story
  */
-export async function GetStory(Astro: Readonly<AstroGlobal>, storySlug: string, storyOptions?: TSOptions) {
+export async function GetStory(Astro: Readonly<AstroGlobal>, storySlug: string, storyOptions?: ISbStoriesParams) {
   let story = null;
   const liveStory = await getLiveStory(Astro);
   const defaultVersion = import.meta.env.DEV ? "draft" : "published";
@@ -28,13 +29,43 @@ export async function GetStory(Astro: Readonly<AstroGlobal>, storySlug: string, 
     resolve_links: 'url',
     ...storyOptions,
   }
-  
   if (liveStory) {
     story = liveStory;
   } else {
     const storyAPI = useStoryblokApi();
-    const { data } = await storyAPI.get(`cdn/stories/${storySlug}`, options );
+    const { data } = await storyAPI.get(`cdn/stories${storySlug}`, options );
     story = data?.story;
   }
+  
   return story;
+}
+
+/**
+ * * FUNCTION: GetStories()
+ * @param Astro | Astro global needed for getLiveStory
+ * @param storyOptions | object
+ * - Expects type TSOptions which takes the same types as ISbStoriesParams 
+ *   just not as many options. storyOptions should be able to overide and add
+ *   as needed. Revise or overide storyOptions type if needed.
+ * @returns stories
+ */
+export async function GetStories(Astro: Readonly<AstroGlobal>, storyOptions?: TSOptions) {
+  let stories = null;
+  const liveStory = await getLiveStory(Astro);
+  const defaultVersion = import.meta.env.DEV ? "draft" : "published";
+
+  const options: ISbStoriesParams = {
+    version: defaultVersion,
+    resolve_links: 'url',
+    ...storyOptions,
+  }
+  if (liveStory) {
+    stories = liveStory;
+  } else {
+    const storyAPI = useStoryblokApi();
+    const { data } = await storyAPI.get(`cdn/stories`, options );
+    stories = data?.stories;
+  }
+  
+  return stories;
 }
